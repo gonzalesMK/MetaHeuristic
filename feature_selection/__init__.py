945fb0ca397d353e594faa556b2ef43fec64c853
gonzalesMK <judcnegri@gmail.com>
2017-09-05 19:35:05 -0300

    implemented Binary Black Hole Algorithm
    it has passed through the check_test of sklearn

--- feature_selection/__init__.py
+++ feature_selection/__init__.py
@@ -1,9 +1,11 @@
 from .harmonic_search import HarmonicSearch
 from .genetic_algorithm import GeneticAlgorithm
 from .random_search import RandomSearch
+from .binary_black_hole import BinaryBlackHole
 
 __all__ = [
         'HarmonicSearch',
         'GeneticAlgorithm',
-        'RandomSearch'
+        'RandomSearch',
+        'BinaryBlackHole'
            ]
