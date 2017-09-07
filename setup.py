64f506b044a5d2180e54a101b8972303606410aa
gonzalesMK <judcnegri@gmail.com>
2017-09-06 22:26:27 -0300

    stable version configuration

--- setup.py
+++ setup.py
@@ -18,15 +18,21 @@ except ImportError:
     print('scipy is required during installation')
     sys.exit(1)
 
+
 setup(name='metaheuristic',
-      version='1.2',
+      version='1.2a1',
       description='A package with some metaheuristics to feature selection',
+      long_description=" Take a look into https://github.com/gonzalesMK/MetaHeuristic",
       author='Juliano D. Negri',
       packages=find_packages(),
       install_requires=INSTALL_REQUIRES,
       author_email='julianonegri@gmail.com',
       url='https://github.com/gonzalesMK/MetaHeuristic',
       classifiers=[
-              'Programming Language :: Python :: 3.6'
+              'Programming Language :: Python :: 3.6',
+              'Development Status :: 3 - Alpha',
+              'Intended Audience :: Science/Research',
+              'Topic :: Scientific/Engineering :: Information Analysis',
+              'Topic :: Software Development :: Libraries'
               ]
       )
