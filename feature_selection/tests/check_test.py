468584a8903295677b1c240b2788658f3409e5f7
gonzalesMK <judcnegri@gmail.com>
2017-09-07 23:21:37 -0300

    new tests
    there is no way to test multiprocessing with deap

--- feature_selection/tests/check_test.py
+++ feature_selection/tests/check_test.py
@@ -6,11 +6,16 @@ from sklearn.svm import SVC
 from feature_selection import HarmonicSearch, GeneticAlgorithm, RandomSearch, BinaryBlackHole, SimulatedAnneling
 from sklearn.utils.testing import assert_raises
 from sklearn.utils.testing import assert_warns
+import multiprocessing
 
+from deap import creator, base
+creator.create("Fitness", base.Fitness, weights=(1.0, -1.0))
+creator.create("Individual", list, fitness=creator.Fitness)
+    
 def test_check_estimator():
     for metaclass in [HarmonicSearch, GeneticAlgorithm, RandomSearch, BinaryBlackHole, SimulatedAnneling]:
         print("check_estimator: ", metaclass()._name)
-        check_estimator(metaclass)        
+        yield check_estimator(metaclass)        
     
 def test_plot():
     dataset = load_breast_cancer()
@@ -49,36 +54,55 @@ def test_plot():
     ga.fit(X, y, normalize=True)
     assert_raises(ValueError, ga.plot_results)
     
-def test_all_prediction():
+#def test_parallel():
+#    dataset = load_breast_cancer()
+#    X, y = dataset['data'], dataset['target_names'].take(dataset['target'])
+#    
+#    # Classifier to be used in the metaheuristic
+#    clf = SVC()
+#    creator.create("Fitness", base.Fitness, weights=(1.0, -1.0))
+#    creator.create("Individual", list, fitness=creator.Fitness)
+#
+#    for metaclass in [HarmonicSearch, GeneticAlgorithm, RandomSearch, BinaryBlackHole, SimulatedAnneling]:
+#        meta = metaclass(classifier=clf, random_state=0, make_logbook=False,
+#                        repeat=2, number_gen=2, parallel=True)
+#        print("Checking parallel ", meta._name)
+#        
+#        # Fit the classifier
+#        meta.fit(X, y, normalize=True)
+#    
+#        # Transformed dataset
+#        X_1 = meta.transform(X)
+#    
+#        meta = metaclass(classifier=clf, random_state=0, make_logbook=False,
+#                        repeat=2, number_gen=2, parallel=True)
+#    
+#        # Fit and Transform
+#        X_2 = meta.fit_transform(X=X, y=y, normalize=True)
+#    
+#        # Check Function
+#        assert_array_equal(X_1, X_2)
+
+def test_score_grid_func():
     dataset = load_breast_cancer()
     X, y = dataset['data'], dataset['target_names'].take(dataset['target'])
     
     # Classifier to be used in the metaheuristic
     clf = SVC()
-    
-    for metaclass in [HarmonicSearch, GeneticAlgorithm, RandomSearch, BinaryBlackHole, SimulatedAnneling]:
-        meta = metaclass(classifier=clf, random_state=0, make_logbook=False,
-                        repeat=2, number_gen=2, predict_with='all')
-        print("Checking all prediction: ", meta._name)
-    # Checks for error
-        assert_raises(ValueError, meta.score_func_to_grid_search, meta, X, y)
+
+    for metaclass in [HarmonicSearch, GeneticAlgorithm, RandomSearch, BinaryBlackHole,SimulatedAnneling]:
+        meta = metaclass(classifier=clf, random_state=0, verbose=50,
+                        make_logbook=True, repeat=1, number_gen=10)
+        
+        print("Checking Grid: ", meta._name)
     
         # Fit the classifier
         meta.fit(X, y, normalize=True)
     
-        # Transformed dataset
-        X_1 = meta.transform(X)
+        # See score 
+        meta.score_func_to_gridsearch(meta)
+            
     
-        meta = metaclass(classifier=clf, random_state=0, make_logbook=False,
-                        repeat=2, number_gen=2, predict_with='all')
-    
-        # Fit and Transform
-        X_2 = meta.fit_transform(X=X, y=y, normalize=True)
-    
-        # Check Function
-        meta.score_func_to_grid_search(meta, X, y)
-        assert_array_equal(X_1, X_2)
-
 def test_unusual_errors():
     dataset = load_breast_cancer()
     X, y = dataset['data'], dataset['target_names'].take(dataset['target'])
@@ -91,7 +115,6 @@ def test_unusual_errors():
                         make_logbook=True, repeat=1, number_gen=1)
         print("Checking unusual erros: ", meta._name)
         meta.fit(X, y, normalize=True)
-    
     
         # Let's suppose you have a empty array 
         meta.best_mask_ = np.array([])
