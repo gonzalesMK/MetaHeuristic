a92e185bb328db28233f87b7da08dc49093f7f65
gonzalesMK <judcnegri@gmail.com>
2017-12-09 15:17:46 -0200

    Changed some code style in BRKGA and SA

--- feature_selection/simulated_anneling.py
+++ feature_selection/simulated_anneling.py
@@ -7,7 +7,7 @@ import numpy as np
 from deap import base
 from deap import tools
 
-import copy 
+import copy
 
 from .base import _BaseMetaHeuristic
 from .base import BaseMask
@@ -16,14 +16,14 @@ from sklearn.utils import check_random_state
 class SimulatedAnneling(_BaseMetaHeuristic):
     """Implementation of a Simulated Anneling Algorithm for Feature Selection as
     stated in the book : Fred W. Glover - Handbook of Metaheuristics.
-    
+
     the decay of the temperature is given by temp_init/number_gen
 
     Parameters
     ----------
     classifier : sklearn classifier , (default=SVM)
-            Any classifier that adheres to the scikit-learn API 
-            
+            Any classifier that adheres to the scikit-learn API
+
     mutation_prob : float in [0,1], (default=0.05)
             Is the the probability for each value in the solution to be mutated
             when searching for some neighbor solution.
@@ -36,47 +36,47 @@ class SimulatedAnneling(_BaseMetaHeuristic):
 
     verbose : boolean, (default=False)
             If true, print information in every generation
-            
+
     repeat : positive int, (default=1)
             Number of times to repeat the fitting process
-    
+
     parallel : boolean, (default=False)
-            Set to True if you want to use multiprocessors            
+            Set to True if you want to use multiprocessors
 
     make_logbook : boolean, (default=False)
             If True, a logbook from DEAP will be made
-            
-    cv_metric_fuction : callable, (default=matthews_corrcoef)            
+
+    cv_metric_fuction : callable, (default=matthews_corrcoef)
             A metric score function as stated in the sklearn http://scikit-learn.org/stable/modules/model_evaluation.html#scoring-parameter
-    
+
     features_metric_function : callable, (default=pow(sum(mask)/(len(mask)*5), 2))
             A function that return a float from the binary mask of features
-            
+
     size_pop: None
-            It is needed to 
+            It is needed to
     """
     def __init__(self, classifier=None, mutation_prob=0.05, initial_temp=10,
                  repetition_schedule=10, number_gen=10, repeat=1, verbose=0,
                  parallel=False, make_logbook=False, random_state=None,
-                 cv_metric_fuction=None, features_metric_function=None, 
+                 cv_metric_fuction=None, features_metric_function=None,
                  **arg):
-    
+
         super(SimulatedAnneling, self).__init__(
                 name = "SimulatedAnneling",
-                classifier=classifier, 
-                number_gen=number_gen,  
+                classifier=classifier,
+                number_gen=number_gen,
                 verbose=verbose,
                 repeat=repeat,
-                parallel=parallel, 
+                parallel=parallel,
                 make_logbook=make_logbook,
                 random_state=random_state,
                 cv_metric_fuction=cv_metric_fuction,
                 features_metric_function=features_metric_function)
-        
+
         self.mutation_prob = mutation_prob
         self.initial_temp = initial_temp
         self.repetition_schedule = repetition_schedule
-        
+
         self.toolbox = base.Toolbox()
         self.toolbox.register("attribute", self._gen_in)
         self.toolbox.register("individual", tools.initIterate,
@@ -90,8 +90,6 @@ class SimulatedAnneling(_BaseMetaHeuristic):
             self.toolbox.register("map", Pool().map)
         else:
             self.toolbox.register("map", map)
-
-
 
     def fit(self, X=None, y=None, normalize=False, **arg):
         """ Fit method
@@ -111,7 +109,7 @@ class SimulatedAnneling(_BaseMetaHeuristic):
                 Set parameters
         """
         initial_time = time.clock()
-        
+
         self.set_params(**arg)
         X,y = self._set_dataset(X=X, y=y, normalize=normalize)
 
@@ -127,28 +125,32 @@ class SimulatedAnneling(_BaseMetaHeuristic):
             hof = tools.HallOfFame(1)
             # Evaluate the solution
             solution.fitness.values = self.toolbox.evaluate(solution)
-            
-            for temp in np.arange(self.initial_temp, 0, - self.initial_temp/self.number_gen):
-                
-                for m in range(self.repetition_schedule): 
-                    
+
+            for temp in np.arange(self.initial_temp, 0,
+                                  - self.initial_temp/self.number_gen):
+
+                for _ in range(self.repetition_schedule):
+
                     prev_solution = copy.deepcopy(solution)
                     self.toolbox.mutate(solution)
                     solution.fitness.values = self.toolbox.evaluate(solution)
-                    
+
                     if prev_solution.fitness > solution.fitness:
-                        solution = self._metropolis_criterion(solution, prev_solution, temp)
-                    
+                        solution = self._metropolis_criterion(
+                                solution, prev_solution, temp)
+
                     # Log statistic
                     hof.update([solution])
-                
+
                 if self.make_logbook:
                     self.logbook[i].record(gen=temp,
                                 best_fit=hof[0].fitness.values[0],
                                 **self.stats.compile([solution]))
                 if self.verbose:
                     #if (int)(temp * self.number_gen/ self.initial_temp) % self.verbose == 0:
-                    print("Repetition:", i+1, "Temperature: ", temp ,  "Elapsed time: ", time.clock() - initial_time, end="\r")
+                    print("Repetition:", i+1, "Temperature: ", temp ,
+                          "Elapsed time: ", time.clock() - initial_time,
+                          end="\r")
 
             best.update(hof)
             if self.make_logbook:
@@ -165,13 +167,14 @@ class SimulatedAnneling(_BaseMetaHeuristic):
 
     @staticmethod
     def _metropolis_criterion(solution, prev_solution, temp):
-        prob = np.exp( (sum(solution.fitness.wvalues) - sum(prev_solution.fitness.wvalues))/temp )
-      
+        prob = np.exp(
+    (sum(solution.fitness.wvalues) - sum(prev_solution.fitness.wvalues))/temp )
+
         if random.random() < prob:
             return solution
         else:
             return prev_solution
-        
+
     def set_params(self, **params):
         super(SimulatedAnneling, self).set_params(**params)
         self.toolbox.register("mutate", tools.mutUniformInt, low=0, up=1,
@@ -181,6 +184,6 @@ class SimulatedAnneling(_BaseMetaHeuristic):
             from multiprocessing import Pool
             self.toolbox.register("map", Pool().map)
         else:
-            self.toolbox.register("map", map)    
-            
+            self.toolbox.register("map", map)
+
         return self
